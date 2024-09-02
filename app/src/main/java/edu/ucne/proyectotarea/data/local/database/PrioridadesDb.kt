6b6a2d303f8c9db410e049d4f6e2b06a00af479d
BrynOvalles <brynovallesburgos@gmail.com>
2024-09-02 00:03:26 -0400

    Creando la base de datos

---
+++ app/src/main/java/edu/ucne/proyectotarea/data/local/database/PrioridadesDb.kt
@@ -0,0 +1,19 @@
+package edu.ucne.proyectotarea.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.proyectotarea.data.local.dao.PrioridadDao
+import edu.ucne.proyectotarea.data.local.entities.PrioridadEntity
+
+@Database(
+    entities = [
+        PrioridadEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+
+
+abstract class PrioridadesDb : RoomDatabase() {
+    abstract fun prioridadDao():  PrioridadDao
+}
