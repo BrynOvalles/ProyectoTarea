0e50d5092f334287d86d37ab475aab06b39bac86
BrynOvalles <brynovallesburgos@gmail.com>
2024-09-02 00:03:11 -0400

    Creando los servicios

---
+++ app/src/main/java/edu/ucne/proyectotarea/data/local/dao/PrioridadDao.kt
@@ -0,0 +1,30 @@
+package edu.ucne.proyectotarea.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.proyectotarea.data.local.entities.PrioridadEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PrioridadDao {
+    @Upsert()
+    suspend fun save(prioridad: PrioridadEntity)
+    @Query(
+        """
+        SELECT *
+        FROM prioridades
+        WHERE priordadId=:id
+        LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): PrioridadEntity?
+
+    @Delete
+    suspend fun delete(prioridad: PrioridadEntity)
+
+    @Query("SELECT * FROM prioridades")
+    fun getAll(): Flow<List<PrioridadEntity>>
+
+}
